9947242aa6e83642d0a65d5c44a14016647d8adb
gezi Zhang <gezizhang@gezideMacBook-Pro.local>
2020-07-04 20:57:43 +0800

    finishtasking_NormalAreaPickBag_should_pick_S_Bag_failed_when_pick_bag_from_PrimaryLockerRobot_given_save_bag_to_Locker_and_use_right_ticket

--- TDDexam/src/test/java/NormalAreaPickBagTest.java
+++ TDDexam/src/test/java/NormalAreaPickBagTest.java
@@ -2,7 +2,6 @@
 //given普通用户存S包然后拿S票找小樱取包，when小樱去Locker取包，then取到用户的S包；
 //given普通用户存M包然后拿M票找小樱取包，when小樱去PrimaryLockerRobot取包，then取到用户的M包；
 // given普通用户存L包然后拿L票找小樱取包，when小樱去SuperLockerRobot取包，then取到自己的L包；
-// givenVIP用户之前找小樱存S包然后再拿正确的票找小樱取包，when小樱去Locker取包，then取到用户的S包；
 // given普通用户拿S票找小樱取包，when小樱去PrimaryLockerRobot和SuperLockerRobot找取包，then取包失败，提示票无效；
 // given普通用户拿假票找小樱取包，when取包，then取包失败，提示票无效；
 // given普通用户拿重复票找小樱取包，when取包，then取包失败，提示票无效；
@@ -14,14 +13,19 @@ import PrimaryLockerRobot.PrimaryLockerRobot;
 import SuperLockerRobot.SuperLockerRobot;
 import exception.InvalidTicketException;
 import exception.LockerNoSpaceException;
+import org.junit.Rule;
 import org.junit.Test;
 import Ticket.Ticket;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
 public class NormalAreaPickBagTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void should_pick_S_Bag_when_pick_bag_given_save_bag_and_use_right_ticket () throws LockerNoSpaceException, InvalidTicketException {
         Bag bag = new Bag();
@@ -51,6 +55,21 @@ public class NormalAreaPickBagTest {
         Ticket ticket=superLockerRobot.SaveBag(bag);
         Bag mybag=superLockerRobot.PickBag(ticket);
         assertEquals(bag,mybag);
+    }
+
+
+    @Test
+    public void should_pick_S_Bag_failed_when_pick_bag_from_PrimaryLockerRobot_given_save_bag_to_Locker_and_use_right_ticket () throws LockerNoSpaceException, InvalidTicketException {
+        Bag bag = new Bag();
+        Locker locker = new Locker("S",3);
+        Locker locker1 = new Locker("M", 4);
+        Locker locker2 = new Locker("M", 4);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(locker1, locker2));
+        Ticket ticket = locker.SaveBag(bag);
+        Bag mybag=primaryLockerRobot.PickBag(ticket);
+        assertEquals(bag,mybag);
+        thrown.expect(InvalidTicketException.class);
+        thrown.expectMessage("fail to save the bag, invaild ticket");
     }
 }
 
